f99f78ff92c39afb8c418878b2b9142604ec2839
Michael Tandy <git@mjt.me.uk>
2018-11-05 22:11:33 +0000

    Early, somewhat tidied up version of demo app supplied with reader.

---
+++ Demo/Date.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+  public  class Date
+  {
+      public string  clhNo { get; set; }
+      public string  clhTagData { get; set; }
+      public string  clhTimes { get; set; }
+    }
+}
